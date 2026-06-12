c781dc7f6a933f6d06bcbc0acc2067e6d2b2e644
Kyrylo Savieliev <k.savieliev@kitsoft.ua>
2026-06-12 14:53:15 +0200

    refactor(admin-front): move action types to a separate variable

--- admin-front/src/application/modules/users/pages/UserLoginJournal/dataTableSettings.js
+++ admin-front/src/application/modules/users/pages/UserLoginJournal/dataTableSettings.js
@@ -11,152 +11,156 @@ import { Link } from 'react-router-dom';
 
 const darkTheme = true;
 
-export default ({ t }) => ({
-  controls: {
-    pagination: true,
-    toolbar: true,
-    search: true,
-    header: true,
-    refresh: true,
-    customizateColumns: true,
-    bottomPagination: true,
-  },
-  checkable: false,
-  darkTheme: darkTheme,
-  columns: [
+export default ({ t }) => {
+  const actionTypes = [
     {
-      id: 'id',
-      name: t('Id'),
+      id: 'login',
+      name: t('LoginAction'),
     },
     {
-      id: 'createdAt',
-      name: t('CreatedAt'),
-      render: (value) => <TimeLabel date={value} />,
+      id: 'logout',
+      name: t('LogoutAction'),
     },
-    {
-      id: 'actionType',
-      name: t('ActionType'),
-    },
-    {
-      id: 'expiresAt',
-      name: t('SessionTime'),
-      render: (value) => <TimeLabel date={value} />,
-    },
-    {
-      id: 'ip',
-      name: t('Ip'),
-      render: (ips) => {
-        if (!Array.isArray(ips)) {
-          return null;
-        }
-        return ips.filter((ip) => ip.indexOf('127.0.0.1') < 0).join(', ');
-      },
-    },
-    {
-      id: 'userName',
-      name: t('UserName'),
-      render: (userName, { userId }) => {
-        if (!userId) {
-          return formatUserName(userName);
-        }
+  ]
 
-        return (
-          <Link to={`/users/#id=${userId}`}>{formatUserName(userName)}</Link>
-        );
+  return {
+    controls: {
+      pagination: true,
+      toolbar: true,
+      search: true,
+      header: true,
+      refresh: true,
+      customizateColumns: true,
+      bottomPagination: true,
+    },
+    checkable: false,
+    darkTheme: darkTheme,
+    columns: [
+      {
+        id: 'id',
+        name: t('Id'),
       },
+      {
+        id: 'createdAt',
+        name: t('CreatedAt'),
+        render: (value) => <TimeLabel date={value} />,
+      },
+      {
+        id: 'actionType',
+        name: t('ActionType'),
+      },
+      {
+        id: 'expiresAt',
+        name: t('SessionTime'),
+        render: (value) => <TimeLabel date={value} />,
+      },
+      {
+        id: 'ip',
+        name: t('Ip'),
+        render: (ips) => {
+          if (!Array.isArray(ips)) {
+            return null;
+          }
+          return ips.filter((ip) => ip.indexOf('127.0.0.1') < 0).join(', ');
+        },
+      },
+      {
+        id: 'userName',
+        name: t('UserName'),
+        render: (userName, { userId }) => {
+          if (!userId) {
+            return formatUserName(userName);
+          }
+
+          return (
+            <Link to={`/users/#id=${userId}`}>{formatUserName(userName)}</Link>
+          );
+        },
+      },
+      {
+        id: 'userId',
+        name: t('UserId'),
+        disableTooltip: true,
+      },
+      {
+        id: 'userAgent',
+        name: t('UserAgent'),
+      },
+      {
+        id: 'clientName',
+        name: t('ClientName'),
+      },
+      {
+        id: 'clientId',
+        name: t('ClientId'),
+      },
+    ],
+    hiddenColumns: ['id', 'userAgent', 'clientId'],
+    filterHandlers: {
+      from_created_at: (props) => (
+        <DateFilterHandler
+          name={t('FromShort')}
+          label={t('FromShort')}
+          darkTheme={darkTheme}
+          {...props}
+        />
+      ),
+      to_created_at: (props) => (
+        <DateFilterHandler
+          name={t('ToShort')}
+          label={t('ToShort')}
+          darkTheme={darkTheme}
+          {...props}
+        />
+      ),
+      userName: (props) => (
+        <StringFilterHandler
+          name={t('UserName')}
+          label={t('UserName')}
+          darkTheme={darkTheme}
+          variant="outlined"
+          {...props}
+        />
+      ),
+      actionType: (props) => (
+        <SelectFilterHandler
+          name={t('ActionType')}
+          label={t('ActionType')}
+          options={actionTypes}
+          darkTheme={darkTheme}
+          listDisplay={true}
+          useOwnNames={true}
+          IconComponent={(props) => <LabelIcon {...props} />}
+          {...props}
+        />
+      ),
+      userId: (props) => (
+        <StringFilterHandler
+          name={t('UserId')}
+          label={t('UserId')}
+          darkTheme={darkTheme}
+          variant="outlined"
+          {...props}
+        />
+      ),
+      ip: (props) => (
+        <StringFilterHandler
+          name={t('Ip')}
+          label={t('Ip')}
+          darkTheme={darkTheme}
+          variant="outlined"
+          {...props}
+        />
+      ),
+      clientId: (props) => (
+        <StringFilterHandler
+          name={t('ClientId')}
+          label={t('ClientId')}
+          darkTheme={darkTheme}
+          variant="outlined"
+          {...props}
+        />
+      ),
     },
-    {
-      id: 'userId',
-      name: t('UserId'),
-      disableTooltip: true,
-    },
-    {
-      id: 'userAgent',
-      name: t('UserAgent'),
-    },
-    {
-      id: 'clientName',
-      name: t('ClientName'),
-    },
-    {
-      id: 'clientId',
-      name: t('ClientId'),
-    },
-  ],
-  hiddenColumns: ['id', 'userAgent', 'clientId'],
-  filterHandlers: {
-    from_created_at: (props) => (
-      <DateFilterHandler
-        name={t('FromShort')}
-        label={t('FromShort')}
-        darkTheme={darkTheme}
-        {...props}
-      />
-    ),
-    to_created_at: (props) => (
-      <DateFilterHandler
-        name={t('ToShort')}
-        label={t('ToShort')}
-        darkTheme={darkTheme}
-        {...props}
-      />
-    ),
-    userName: (props) => (
-      <StringFilterHandler
-        name={t('UserName')}
-        label={t('UserName')}
-        darkTheme={darkTheme}
-        variant="outlined"
-        {...props}
-      />
-    ),
-    actionType: (props) => (
-      <SelectFilterHandler
-        name={t('ActionType')}
-        label={t('ActionType')}
-        options={[
-          {
-            id: 'login',
-            name: t('LoginAction'),
-          },
-          {
-            id: 'logout',
-            name: t('LogoutAction'),
-          },
-        ]}
-        darkTheme={darkTheme}
-        listDisplay={true}
-        useOwnNames={true}
-        IconComponent={(props) => <LabelIcon {...props} />}
-        {...props}
-      />
-    ),
-    userId: (props) => (
-      <StringFilterHandler
-        name={t('UserId')}
-        label={t('UserId')}
-        darkTheme={darkTheme}
-        variant="outlined"
-        {...props}
-      />
-    ),
-    ip: (props) => (
-      <StringFilterHandler
-        name={t('Ip')}
-        label={t('Ip')}
-        darkTheme={darkTheme}
-        variant="outlined"
-        {...props}
-      />
-    ),
-    clientId: (props) => (
-      <StringFilterHandler
-        name={t('ClientId')}
-        label={t('ClientId')}
-        darkTheme={darkTheme}
-        variant="outlined"
-        {...props}
-      />
-    ),
-  },
-});
+  }
+};
