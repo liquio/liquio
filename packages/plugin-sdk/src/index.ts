e6b3893b3ab2eeb5fd6c4137acba8a7bd8f05afe
Andrii Arsenin <a.arsenin@kitsoft.ua>
2026-07-29 04:47:25 -0400

    feat(plugin-sdk): wire up barrel export
    Replaces the placeholder export with the package's full public API:
    manifest validation, BasePlugin and both provider base classes, loader
    types/registry/loader, and error classes. @liquio/plugin-sdk 0.1.0 is
    now ready to publish.

--- packages/plugin-sdk/src/index.ts
+++ packages/plugin-sdk/src/index.ts
@@ -1,1 +1,8 @@
-export {};
+export * from "./manifest/plugin_manifest";
+export * from "./providers/base_provider";
+export * from "./providers/event_external_service_provider";
+export * from "./providers/external_reader_provider";
+export * from "./loader/types";
+export * from "./loader/plugin_registry";
+export * from "./loader/plugin_loader";
+export * from "./errors/plugin_errors";
